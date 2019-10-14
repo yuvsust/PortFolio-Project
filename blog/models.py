998aac1f68882a5db389a1296c620d8f222f1cb2
yuv <tanvirhossainyuvraz@gmail.com>
2019-10-15 02:23:48 +0600

    Blog Model is created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Create a Blog Models  (Create a class actually)
+# Blog models should have:
+    # title
+    # publication_date
+    # body  ( Text )
+    # images
+
+class Blog (models.Model):
+    title = models.CharField('Title', max_length = 40)
+    pub_date = models.DateTimeField('Post Date')
+    body = models.TextField(blank = True)
+    image = models.ImageField(upload_to = 'images/')
